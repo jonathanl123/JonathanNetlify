cf3e113fbc53821306bfaf3e8867f562da4bdc42
jonathanl123 <62575455+jonathanl123@users.noreply.github.com>
2021-04-30 12:01:39 +0200

    Add files via upload

--- japonais/hiragana/script.js
+++ japonais/hiragana/script.js
@@ -9,13 +9,6 @@ function cacher(){
 }
 
 
-//function aleat(){
-//    var caract = document.getElementById("validation").getElementsByTagName("div");
-//    caract.forEach(function(groupe){
-//        caract.insertBefore(groupe, caract[Math.floor(Math.random()*caract.length)]);
-//  });
-//}
-
 function aleat(){
     const caract = ["あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","を","ん"];
     
@@ -25,23 +18,17 @@ function aleat(){
  
     console.log(huitAleatoires)
     document.getElementById("textAleat").innerHTML = huitAleatoires.join();
-
-//    const randomCaract = caract[Math.floor(Math.random() * caract.length)];
-//    console.log("random caract =>", randomCaract);
-//    var elt = document.getElementById('tt');
-//    var monTexte = elt.innerText;
-//    console.log(monTexte);
 }
 
 
-$(function() {
+function divAleat(){
   var parent = $(".divparent");
 
   var divs = parent.children();
     while (divs.length) {
         parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
     }
-});
+};
 
 
 
